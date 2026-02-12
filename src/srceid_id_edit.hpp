b54e934e9397c04de438b01ba10dc1e85e4b5ab8
zen4215 <44k1.373@gmail.com>
2026-02-12 16:58:22 +0000

    feat: some

--- src/srceid_id_edit.hpp
+++ src/srceid_id_edit.hpp
@@ -6,6 +6,11 @@ namespace NNCT {
 namespace SRCEID {
 
 struct Identifier {
+    Identifier();
+    Identifier(uint8_t sender_id, uint8_t receiver_id) {
+        senderID    = sender_id;
+        receiverID  = receiver_id;
+    }
     uint8_t  senderID;
     uint8_t  receiverID;
     bool     coreCommandFlag;
@@ -14,10 +19,10 @@ struct Identifier {
 
 Identifier perse_id(uint32_t id) {
     Identifier ret_id;
-    ret_id.senderID = id >> 21;
-    ret_id.receiverID = (id >> 13) & 0xFF;
-    ret_id.coreCommandFlag = (id >> 12) & 0x1;
-    ret_id.command = id & 0xFFF;
+    ret_id.senderID         = id >> 21;
+    ret_id.receiverID       = (id >> 13) & 0xFF;
+    ret_id.coreCommandFlag  = (id >> 12) & 0x1;
+    ret_id.command          = id & 0xFFF;
 }
 
 uint32_t merge_id(Identifier id) {
